4abf7ded400ac0092b841efbe7769bbcfb4ad34e
truongcom <truongnqyh@gmail.com>
2024-05-24 04:35:47 -0700

    update shared memory

---
+++ IPC_udm/Shared_Memory/Shm_ram_only/shm_ram.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <memory.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]){
+    
+    return 0;
+}
